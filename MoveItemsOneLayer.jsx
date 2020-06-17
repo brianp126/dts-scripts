8b853b09a50ddd10bcf53dd25fa47cd528d34920
Brian Pifer <bpifer311@hotmail.com>
2020-06-16 22:18:09 -0700

    Create MoveItemsOneLayer.jsx

---
+++ MoveItemsOneLayer.jsx
@@ -0,0 +1,23 @@
+/*
+MoveItemsOneLayer.jsx
+Author: Brian Pifer, www.designtimesolutions.com
+Two scripts, one to move selected items down a layer, the other to move them up a layer. Save as two separate scripts. 
+*/
+
+//move down a layer
+var sel = app.selection;
+var doc = app.activeDocument;
+for (var i = 0; i < sel.length; i++) {
+    try {
+        sel[i].move(doc.layers.nextItem(sel[i].itemLayer));
+    } catch(e) {}
+}
+
+//move up a layer
+var sel = app.selection;
+var doc = app.activeDocument;
+for (var i = 0; i < sel.length; i++) {
+    try {
+        sel[i].move(doc.layers.previousItem(sel[i].itemLayer));
+    } catch(e) {}
+}
